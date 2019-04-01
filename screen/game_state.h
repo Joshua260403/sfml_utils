5383d90cce615b5106d3e00cd8c92ee5a18c1f9c
Joshua260403 <joshuavwaardenberg@gmail.com>
2019-04-01 13:29:45 +0200

    game states
    enum class game state to go with the window manager

---
+++ screen/game_state.h
@@ -0,0 +1,15 @@
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
+
+enum class game_state
+{
+  titlescreen,
+  menuscreen,
+  aboutscreen,
+  playing,
+  gameover,
+  paused,
+  saving
+};
+
+#endif // GAME_STATE_H
